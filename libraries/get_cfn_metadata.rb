f87c0faca9f36bc2e9b9472631536ab831cdb293
Kevin Young <kevin_young@intuit.com>
2013-07-19 15:36:42 -0700

    updates

--- libraries/get_cfn_metadata.rb
+++ libraries/get_cfn_metadata.rb
@@ -1,7 +1,5 @@
-class Chef::Recipe::CfnMetadataLoader
+class CfnMetadataLoader
 
-  IGNORED_METADATA_KEYS = [ 'AWS::CloudFormation::Init',
-                            'AWS::CloudFormation::Authentication' ]
   def initialize(stack_name, region, resource_name, access_key, secret_key, cfn_path)
     @stack_name = stack_name
     @region = region
@@ -9,17 +7,18 @@ class Chef::Recipe::CfnMetadataLoader
     @access_key = access_key
     @secret_key = secret_key
     @cfn_path = cfn_path
-
   end
 
   def sanitized_metadata
-    data = raw_cfn_metadata.reject { |k,v| IGNORED_METADATA_KEYS.include? k }
-    data = data.to_hash.select { |k, v| k =~ /app_/}
-    data = organize_cfn_metadata data
-    data
+    raw_cfn_metadata.reject { |k,v| ignored_metadata_keys.include? k }
   end
 
   private
+
+  def ignored_metadata_keys
+    [ 'AWS::CloudFormation::Init','AWS::CloudFormation::Authentication' ]
+  end
+
   def raw_cfn_metadata
     output = `#{cfn_metadata_command}`
     raise RuntimeError 'Unable to get cloud formation metadata' unless $?.success?
@@ -33,18 +32,6 @@ class Chef::Recipe::CfnMetadataLoader
     cmd << "--region #{@region} "
     cmd << "--access-key #{@access_key} "
     cmd << "--secret-key #{@secret_key}"
-  end
-  
-  def organize_cfn_metadata(app_vars)
-    app_vars.each_pair do |k,v|
-      app_vars[k] = v.join(',') if v.kind_of? Enumerable
-    end 
-
-    app_vars.keys.each do |key|
-     app_vars[key.gsub('app_', '')] = app_vars[key]
-       app_vars.delete key
-    end
-    app_vars
   end
 
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,8 @@
-cfn = CfnMetadataLoader.new(node['cfn_metadata']['stack_name'],
+cfn = CfnMetadataLoader.new node['cfn_metadata']['stack_name'],
                             node['cfn_metadata']['region'],
                             node['cfn_metadata']['resource_name'],
                             node['cfn_metadata']['access_key'],
                             node['cfn_metadata']['secret_key'],
-                            node['cfn_metadata']['cfn_path'])
+                            node['cfn_metadata']['cfn_get_metadata_bin']
 
 node.set['cfn_metadata']['data'] = cfn.sanitized_metadata
